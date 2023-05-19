c3a46d3ec562eb8f01ad5a960e503b13ff0c6bfd
Abhinav <84911433+Abhivyaktii@users.noreply.github.com>
2023-05-19 14:22:11 +0530

    Time: 54 ms (79.62%), Space: 41.8 MB (74.58%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -5,10 +5,10 @@
  */
 var filter = function(arr, fn) {
     // return arr.map(item=> fn(item))
-    const result = [];
-    arr.forEach((item,index) => {
-        if (fn(item, index)) result.push(item);
+    // const result = [];
+    return arr.reduce((acc,item,index) => {
+        if (fn(item, index)) acc.push(item);
+        return acc;
         // result.push(fn(item, index));
-    });
-    return result;
+    }, []);
 };
